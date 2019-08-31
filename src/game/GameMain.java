704f49573f0dd8107e82980d22db7cd9ffa44613
Ha Nguyen <hn47@@duke.edu>
2019-08-31 10:58:45 -0400

    Added event handler for paddle

--- src/game/GameMain.java
+++ src/game/GameMain.java
@@ -1,5 +1,7 @@
 package game;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -7,6 +9,7 @@ import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.File;
 import java.util.Scanner;
@@ -14,7 +17,7 @@ import java.util.stream.Stream;
 
 public class GameMain extends Application {
     public static final Paint LEVEL_ONE_BACKGROUND = Color.DARKGRAY.darker().darker();
-    public static final Paint LEVEL_TWO_BACKGROUND = Color.SADDLEBROWN.darker().darker().darker().darker();
+    public static final Paint LEVEL_TWO_BACKGROUND = Color.BLACK;
     public static final Paint LEVEL_THREE_BACKGROUND = Color.DARKBLUE.darker().darker();
     public static final String LEVEL_ONE_BRICKS_CONFIG_PATH = "resources/level1.txt";
     public static final String LEVEL_TWO_BRICKS_CONFIG_PATH = "resources/level2.txt";
@@ -36,89 +39,21 @@ public class GameMain extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         myStage = stage;
-        myLevelOneScene = setupLevelOne();
-        myLevelTwoScene = setupLevelTwo();
-        myLevelThreeScene = setupLevelThree();
+        Level levelOne = new Level(myStage, LEVEL_ONE_BRICKS_CONFIG_PATH, LEVEL_ONE_BACKGROUND);
+        Level levelTwo = new Level(myStage, LEVEL_TWO_BRICKS_CONFIG_PATH, LEVEL_TWO_BACKGROUND);
+        Level levelThree = new Level(myStage, LEVEL_THREE_BRICKS_CONFIG_PATH, LEVEL_THREE_BACKGROUND);
+
+        myLevelOneScene = levelOne.getScene();
+        myLevelTwoScene = levelTwo.getScene();
+        myLevelThreeScene = levelThree.getScene();
+
+        levelOne.setNextScene(myLevelTwoScene);
+        levelTwo.setNextScene(myLevelThreeScene);
+        levelThree.setNextScene(myLevelOneScene);
 
         stage.setScene(myLevelOneScene);
         stage.setTitle("Game");
         stage.show();
-    }
-
-    private Scene setupLevelOne() throws Exception {
-        Group levelOneRoot = new Group();
-        setupBricksConfig(LEVEL_ONE_BRICKS_CONFIG_PATH, levelOneRoot);
-        setupGameElements(levelOneRoot);
-
-        Button button = new Button("Go to scene 2");
-        button.setOnAction(e -> myStage.setScene(myLevelTwoScene));
-        levelOneRoot.getChildren().add(button);
-
-        Scene levelOneScene = new Scene(levelOneRoot, SCENE_WIDTH, SCENE_HEIGHT, LEVEL_ONE_BACKGROUND);
-        return levelOneScene;
-    }
-
-    private Scene setupLevelTwo() throws Exception{
-        Group levelTwoRoot = new Group();
-
-        setupBricksConfig(LEVEL_TWO_BRICKS_CONFIG_PATH, levelTwoRoot);
-        setupGameElements(levelTwoRoot);
-
-        Button button = new Button("Go to scene 3");
-        button.setOnAction(e -> myStage.setScene(myLevelThreeScene));
-        levelTwoRoot.getChildren().add(button);
-
-        Scene levelTwoScene = new Scene(levelTwoRoot, SCENE_WIDTH, SCENE_HEIGHT, LEVEL_TWO_BACKGROUND);
-        return levelTwoScene;
-    }
-
-    private Scene setupLevelThree() throws Exception {
-        Group levelThreeRoot = new Group();
-
-        setupBricksConfig(LEVEL_THREE_BRICKS_CONFIG_PATH, levelThreeRoot);
-        setupGameElements(levelThreeRoot);
-
-        Button button = new Button("Go to scene 1");
-        button.setOnAction(e -> myStage.setScene(myLevelOneScene));
-        levelThreeRoot.getChildren().add(button);
-
-        Scene levelThreeScene = new Scene(levelThreeRoot, SCENE_WIDTH, SCENE_HEIGHT, LEVEL_THREE_BACKGROUND);
-        return levelThreeScene;
-    }
-
-    private void setupBricksConfig(String path, Group root) throws Exception {
-        File bricksConfig = new File(path);
-        Scanner scanner = new Scanner(bricksConfig);
-        int yCoordinateForRow = 0;
-
-        while (scanner.hasNextLine()) {
-            String rowConfiguration = scanner.nextLine();
-            yCoordinateForRow += BRICK_HEIGHT;
-            setRow(root, rowConfiguration, yCoordinateForRow);
-        }
-        scanner.close();
-    }
-
-    private void setRow(Group root, String rowConfiguration, int yCoordinateForRow) {
-        int[] rowConfigurationArray = Stream.of(rowConfiguration.split(" " )).mapToInt(Integer::parseInt).toArray();
-        int xCoordinateForBrick = 0;
-        for (int i=0; i<rowConfigurationArray.length; i++) {
-            Brick brick = new Brick(rowConfigurationArray[i]);
-            brick.getBricksImageView().setX(xCoordinateForBrick);
-            brick.getBricksImageView().setY(yCoordinateForRow);
-            xCoordinateForBrick += BRICK_WIDTH;
-            root.getChildren().add(brick.getBricksImageView());
-        }
-    }
-
-    private void setupGameElements(Group root) {
-        Character harryPotter = new Character(Character.CharacterEnum.HARRY_POTTER);
-        harryPotter.setCharacterAsPaddle(root);
-        Ball mainBall = new Ball();
-        mainBall.resetBall(harryPotter);
-        mainBall.addBallToScreen(root);
-        Structure door = new Structure(Structure.StructureEnum.DOOR);
-        door.setDoor(root);
     }
 
 }
---
+++ src/game/Level.java
@@ -0,0 +1,86 @@
+package game;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Level {
+    private Stage myStage;
+    private Scene myScene;
+    private Character myMainCharacter;
+    private Scene myNextScene;
+
+    public Level(Stage stage, String bricksConfigPath, Paint background) throws Exception {
+        myStage = stage;
+        myScene = setupScene(bricksConfigPath, background);
+    }
+
+    public Scene getScene() {
+        return myScene;
+    }
+
+    public Character getMainCharacter() {
+        return myMainCharacter;
+    }
+
+    public void setNextScene(Scene scene) {
+        myNextScene = scene;
+    }
+
+    private Scene setupScene(String bricksConfigPath, Paint background) throws Exception {
+        Group root = new Group();
+        setupBricksConfig(bricksConfigPath, root);
+
+        myMainCharacter = new Character(Character.CharacterEnum.HARRY_POTTER);
+        myMainCharacter.setCharacterAsPaddle(root);
+
+        Ball mainBall = new Ball();
+        mainBall.resetBall(myMainCharacter);
+        mainBall.addBallToScreen(root);
+        Structure door = new Structure(Structure.StructureEnum.DOOR);
+        door.setDoor(root);
+
+        Button button = new Button("Go to next scene");
+        button.setOnAction(e -> myStage.setScene(myNextScene));
+        root.getChildren().add(button);
+
+        Scene scene = new Scene(root, GameMain.SCENE_WIDTH, GameMain.SCENE_HEIGHT, background);
+        scene.setOnMouseMoved(e -> handleMouseInput(e.getX()));
+        return scene;
+    }
+
+    private void setupBricksConfig(String path, Group root) throws Exception {
+        File bricksConfig = new File(path);
+        Scanner scanner = new Scanner(bricksConfig);
+        int yCoordinateForRow = 0;
+
+        while (scanner.hasNextLine()) {
+            String rowConfiguration = scanner.nextLine();
+            yCoordinateForRow += Brick.BRICK_HEIGHT;
+            setRow(root, rowConfiguration, yCoordinateForRow);
+        }
+        scanner.close();
+    }
+
+    private void setRow(Group root, String rowConfiguration, int yCoordinateForRow) {
+        int[] rowConfigurationArray = Stream.of(rowConfiguration.split(" " )).mapToInt(Integer::parseInt).toArray();
+        int xCoordinateForBrick = 0;
+        for (int i=0; i<rowConfigurationArray.length; i++) {
+            Brick brick = new Brick(rowConfigurationArray[i]);
+            brick.getBricksImageView().setX(xCoordinateForBrick);
+            brick.getBricksImageView().setY(yCoordinateForRow);
+            xCoordinateForBrick += Brick.BRICK_WIDTH;
+            root.getChildren().add(brick.getBricksImageView());
+        }
+    }
+
+    private void handleMouseInput(double x) {
+        myMainCharacter.getCharacterImageView().setX(x);
+    }
+}
